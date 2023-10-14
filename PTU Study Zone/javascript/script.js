38c8e75f5202703d9b9c644b560e99ceb55d9ff2
Vikash Bharal <139474262+Vikash4110@users.noreply.github.com>
2023-10-14 23:33:19 +0530

    Add files via upload

---
+++ PTU Study Zone/javascript/script.js
@@ -0,0 +1,9 @@
+var typed=new Typed(".text",{
+    strings:["Physics" , "Mathematics" , "PPS" , "BEE"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+    }
+    )
+    
